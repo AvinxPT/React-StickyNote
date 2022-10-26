59af6a42af392abce8b82d2dbd209cee6509fa87
AvinxPT <45512358+AvinxPT@users.noreply.github.com>
2022-10-26 17:37:38 +0100

    update Header.jsx
    added onClick event.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 const Header = () => {
+  const clickHandler = (e) => {
+    e.preventDefault();
+    alert("Clicked");
+  };
+
   return (
     <section className="header">
       <div>Note do fabinho</div>
       <div className="header-actions">
-        <span className="header-actions-register">Add new note</span>
+        <span
+          className="header-actions-register"
+          onClick={() => alert("Clicked")}
+        >
+          Add new note
+        </span>
       </div>
     </section>
   );
